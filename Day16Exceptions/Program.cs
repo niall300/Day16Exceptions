83f7b45017cb88cbba83fef5ef210638313f6abe
niall coffey <niall300@yahoo.co.uk>
2017-04-13 11:13:51 +0100

    basic exception handling

--- Day16Exceptions/Program.cs
+++ Day16Exceptions/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Read a string, , and print its integer value;
+//    if  cannot be converted to an integer, print Bad String.
 namespace Day16Exceptions
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string S = Console.ReadLine();
+            try
+            {
+                int num = Int32.Parse(S);
+
+            }
+            catch (FormatException)
+            {
+
+                Console.WriteLine("Bad String");
+                Console.ReadLine();
+            }
+
         }
     }
 }
